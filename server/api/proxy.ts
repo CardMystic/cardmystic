1633ee0b3b9dd532a050045befb491f282c613e9
Dark Mode <darkkmodee00@gmail.com>
2025-02-20 17:44:40 -0500

    Test url

--- server/api/proxy.ts
+++ server/api/proxy.ts
@@ -1,6 +1,8 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
+  console.log('url: ', `${process.env.BACKEND_URL}:${process.env.BACKEND_PORT}/search`)
+
   const response = await $fetch(`${process.env.BACKEND_URL}:${process.env.BACKEND_PORT}/search`, {
     method: 'POST',
     body: body
