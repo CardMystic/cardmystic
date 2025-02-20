0676d2b8f72275022ae1dd601348f75610d6a904
Dark Mode <darkkmodee00@gmail.com>
2025-02-20 17:28:59 -0500

    Updates backend url env variable

--- server/api/proxy.ts
+++ server/api/proxy.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
-  const response = await $fetch(`http://${process.env.BACKEND_URL}:${process.env.BACKEND_PORT}/search`, {
+  const response = await $fetch(`${process.env.BACKEND_URL}:${process.env.BACKEND_PORT}/search`, {
     method: 'POST',
     body: body
   });
