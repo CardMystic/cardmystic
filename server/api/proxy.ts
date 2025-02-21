78c1ebc8e0bbd401a2ef14b584cc480c7143a58d
Dark Mode <darkkmodee00@gmail.com>
2025-02-21 11:22:22 -0500

    Tests hardcoded url

--- server/api/proxy.ts
+++ server/api/proxy.ts
@@ -1,9 +1,7 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
-  console.log('url: ', `${process.env.NUXT_PUBLIC_API_URL}:${process.env.NUXT_PUBLIC_API_PORT}/search`)
-
-  const response = await $fetch(`${process.env.NUXT_PUBLIC_API_URL}:${process.env.NUXT_PUBLIC_API_PORT}/search`, {
+  const response = await $fetch(`https://api.cardmystic.io/search`, {
     method: 'POST',
     body: body
   });
