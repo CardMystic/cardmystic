8a797048cb97a559fed21886141e1698f3d05d02
Dark Mode <darkkmodee00@gmail.com>
2025-03-04 15:28:22 -0500

    Adds vercel analytics

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Analytics></Analytics>
     <v-app  class="gradient-background">
       <NuxtLayout >
         <NuxtPage />
@@ -13,3 +14,8 @@
   min-height: 100vh !important
   background: linear-gradient(180deg, #1E1E1E, #9A499A) !important
 </style>
+
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+
+</script>
