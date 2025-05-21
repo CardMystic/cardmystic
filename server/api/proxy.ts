e919f632b5b48290e5062bfc3e64bd6a299e5958
Dark Mode <darkkmodee00@gmail.com>
2025-05-21 17:53:36 -0400

    Api proxy

--- server/api/proxy.ts
+++ server/api/proxy.ts
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
-  const response = await $fetch(`http://localhost:3000/search`, {
+  const response = await $fetch(`https://api.cardmystic.io/search`, {
     method: 'POST',
     body: body,
   });
