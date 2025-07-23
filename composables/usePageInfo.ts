21acb746cdaf4cf51e9e63a0c6022832717f5b3c
Julian <julian@jpinzer.me>
2025-07-23 12:05:22 -0400

    fix(usePageInfo): simplify getPageInfo function signature

--- composables/usePageInfo.ts
+++ composables/usePageInfo.ts
@@ -8,7 +8,7 @@ export const usePageInfo = () => {
     pageInfo.value = info;
   };
 
-  const getPageInfo: () => PageInfo | undefined = () => pageInfo.value;
+  const getPageInfo = () => pageInfo.value;
 
   return {
     pageInfo: readonly(pageInfo),
