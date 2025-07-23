104adae5d62bb2538b926b72a659cae8e5d77687
Julian <julian@jpinzer.me>
2025-07-23 12:05:15 -0400

    feat(IssuesFab): Add an onclick prop for the FAB

--- components/search/IssuesFab.vue
+++ components/search/IssuesFab.vue
@@ -1,13 +1,17 @@
 <template>
   <v-tooltip text="Report an issue with these results">
     <template v-slot:activator="{ props }">
-      <v-fab v-bind="props" color="surface-variant" icon="mdi-alert-circle-outline" class="fixed-fab"></v-fab>
+      <v-fab v-bind="props" color="surface-variant" icon="mdi-alert-circle-outline" class="fixed-fab"
+        @click="onClick && onClick()"></v-fab>
     </template>
   </v-tooltip>
 </template>
 
 <script lang="ts" setup>
-
+const props = defineProps<{
+  onClick?: () => void
+}>();
+const onClick = props.onClick;
 </script>
 
 <style lang="sass" scoped>
