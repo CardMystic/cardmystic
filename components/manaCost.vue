fe211309e608b1ffcdb4c2b7e4348ff12802d818
Dark Mode <darkkmodee00@gmail.com>
2025-07-28 20:25:41 -0400

    Creates a ManaCost component

---
+++ components/manaCost.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+
+import { formatSymbols } from '~/utils/formatMTGSymbols';
+
+const props = defineProps({
+  manaCost: {
+    type: String,
+    required: false,
+  },
+});
+
+const formattedManaCost = computed(() => {
+  return formatSymbols(props.manaCost);
+});
+
+</script>
+<template>
+  <div>
+    <template v-for="(part, index) in formattedManaCost" :key="index">
+      <template v-if="typeof part === 'string'">{{ part }}</template>
+      <component v-else :is="part" />
+    </template>
+  </div>
+</template>
+
+<style scoped lang="sass">
+</style>
