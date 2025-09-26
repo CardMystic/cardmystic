6320373706cf1187ce5cc14701fc2e694485c19f
Dark Mode <148381660+imdarkmode@users.noreply.github.com>
2025-09-26 11:06:55 -0400

    Andrew/clipboard (#108)
    * Added plus button on card, appears on hover
    
    * Always show plus signs on mobile screens
    
    * Add button is neutral color
    
    * Reduces bottom margin on filters
    
    * Adds a share results fab
    
    * Moves report results fab above results next to share results
    
    * Fabs have cursor pointer
    
    * Moves the score button and adds thumbs up and down buttons
    
    * Adds tooltips to thumbs up/down buttons
    
    * Reduces card.vue padding
    
    * Adds issuesFab and shareFab to similarity results
    
    * Removes thumbs up/down buttons from example query on home page
    
    * Adds clipboard composable and lets user add and remove cards
    
    * Slight update to clipbaord add button styling
    
    * Adds clipboard menu
    
    * Adds mass entry affiliate link for clipboard cards
    
    * Cursor pointers
    
    * Fixes pointer events on checkmark button to remove from clipboard
    
    * Adds a v-if on issuesFab to show only when results are present
    
    * Fixes query not being stored in sessionStorage if queried form home page
    
    * Clicking on the thumbs up/down buttons toggles their colors, disabled
    
    * Adds slight margin to filters chips
    
    * Allows scroll in show card data for dev
    
    * Dev card data shown fixed

--- pages/search/similarity.vue
+++ pages/search/similarity.vue
@@ -3,6 +3,11 @@
     <div class="w-full max-w-7xl px-4 pt-4 flex flex-col items-center">
       <SearchForm similarity class="mt-6 w-full" />
 
+      <div class="w-full flex justify-end min-h-[32px]">
+        <ShareFab class="mr-2" v-if="searchResults && searchResults.length"></ShareFab>
+        <IssuesFab v-if="searchResults && searchResults.length" :onClick="handleFabClick" />
+      </div>
+
       <!-- Results -->
       <div class="mt-3 w-full">
         <template v-if="isLoading">
@@ -45,7 +50,6 @@
       </div>
     </div>
   </UContainer>
-  <IssuesFab :onClick="handleFabClick" />
 </template>
 
 <script setup lang="ts">
@@ -53,10 +57,10 @@ import { useQuery } from '@tanstack/vue-query';
 import { ref, watch, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import type { Card } from '~/models/cardModel';
-
 import { CardSearchFiltersSchema, SimilaritySearchSchema } from '~/models/searchModel';
 import SearchForm from '~/components/search/Search.vue';
 import IssuesFab from '~/components/search/IssuesFab.vue';
+import ShareFab from '~/components/search/shareFab.vue';
 import CardSkeleton from '~/components/CardSkeleton.vue';
 import searchFeedbackUrl from '~/utils/searchFeedbackUrl';
 import CardComponent from '~/components/card.vue';
