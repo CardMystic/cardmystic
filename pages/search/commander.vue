cee9caa852d31b7bc2b2573210ac21d5a6a7c30a
Andrew Morrison <148381660+imdarkmode@users.noreply.github.com>
2025-12-20 12:33:31 -0500

    Sort spacer prevents CLS (#165)

--- pages/search/commander.vue
+++ pages/search/commander.vue
@@ -6,6 +6,7 @@
       <!-- Results -->
       <div class="mt-3 w-full">
         <template v-if="isLoading">
+          <div style="height: 32px"></div> <!-- Sort spacer to prevent layout shift -->
           <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
             <CardSkeleton v-for="i in skeletonCount" :key="`skeleton-${i}`" :showCardInfo="true" />
           </div>
