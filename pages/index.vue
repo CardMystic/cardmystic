259e6cd310265d662347cb2a7a27557f1566f9e6
Andrew Morrison <darkkmodee00@gmail.com>
2026-01-31 10:39:49 -0500

    dont import search form

--- pages/index.vue
+++ pages/index.vue
@@ -126,7 +126,6 @@ useHead({
   ]
 })
 
-import SearchForm from '~/components/search/Search.vue';
 import ProductPromotionButtons from '~/components/ProductPromotionButtons.vue';
 import CardSimple from '~/components/CardSimple.vue';
 // import RecentLists from '~/components/RecentLists.vue';
