8e05e673844398ebfeaee26ccd9121147edb1659
Andrew Morrison <darkkmodee00@gmail.com>
2026-03-04 15:45:56 -0500

    Adds more stars, login tooltip min width

--- components/user/LoginTooltip.vue
+++ components/user/LoginTooltip.vue
@@ -21,9 +21,9 @@ function dismissTooltip() {
 
 <template>
   <div v-if="showTooltip"
-    class="absolute top-full right-0 mt-4 w-64 bg-secondary text-white p-3 rounded-lg shadow-lg z-50 flex flex-row items-center">
+    class="min-w-[256px] absolute top-full right-0 mt-4 w-64 bg-secondary text-white p-3 rounded-lg shadow-lg z-50 flex flex-row items-center">
 
-    <div class="text-sm font-medium">
+    <div class="text-[13px] font-medium">
       <span class="font-bold animate-rainbow">NEW!</span> Create an account for card lists, search history, and more!
     </div>
     <UButton @click="dismissTooltip" icon="i-lucide-x" size="xs" color="neutral" variant="solid"
